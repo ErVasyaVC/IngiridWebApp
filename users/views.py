b24cfacc9ce9eb10d00b4dfae5ba78fdd661e3df
Vasya Eremin <ervasyavc@gmail.com>
2024-05-14 19:08:07 +0300

    login create

--- users/views.py
+++ users/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponsePermanentRedirect
+from django.urls import reverse
+from django.contrib import auth
 
-# Create your views here.
+from users.forms import UserLoginForm
+
+
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user and user.is_active:
+                auth.login(request, user)
+                return HttpResponsePermanentRedirect('/')
+    else:
+        form = UserLoginForm()
+    context = {'form': form}
+    return render(request, 'users/login.html', context)
+
+
+def register(request):
+    return render(request, 'users/register.html')
